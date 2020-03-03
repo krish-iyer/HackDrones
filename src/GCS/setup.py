9e5a5d957791c6deb61311f7744c74922002dbf8
Krishnan <krishnaniyer97@gmail.com>
2020-03-03 14:52:05 +0530

    create installation package of the library

---
+++ src/GCS/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="MultiWii",
+    description="MultiWii Package (MSP) to send commands and requests, and receive response over UDP ",
+    version="1",
+    url="https://github.com/krish-iyer/hackdrones/",
+    author="B Krishnan Iyer",
+    license="GPL",
+    packages=['MultiWii'],
+)
